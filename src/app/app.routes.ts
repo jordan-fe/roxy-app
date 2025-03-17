4eeb7cb29608ae0f5281395799abbc5389b3cf67
j.michel <j.michel@wavenet.be>
2025-03-17 19:37:12 +0100

    Add terms page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import {ContactComponent} from "./contact/contact.component";
 import {HomeComponent} from "./home/home.component";
 import {GalerieComponent} from './galerie/galerie.component';
 import {MyServicesComponent} from './my-services/my-services.component';
+import {TermsComponent} from "./terms/terms.component";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'mes-services', component: MyServicesComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'galerie', component: GalerieComponent },
+  { path: 'terms', component: TermsComponent },
   { path: '**', redirectTo: '' }
 ];
---
+++ src/app/terms/terms.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-terms',
+  imports: [],
+  templateUrl: './terms.component.html',
+  standalone: true,
+  styleUrl: './terms.component.scss'
+})
+export class TermsComponent {
+
+}
